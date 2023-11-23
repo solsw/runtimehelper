07f2767c34e96e2cce508331ac5b084f3e151d20
Oleg Subachev <git@solsw.ru>
2023-11-23 10:28:02 +0500

    NthCallerName added

--- runtimehelper.go
+++ runtimehelper.go
@@ -4,8 +4,10 @@ import (
 	"runtime"
 )
 
-// nthCallerName returns name of the n-th caller function of nthCallerName.
-func nthCallerName(n int) string {
+// NthCallerName returns name of the n-th caller function of NthCallerName.
+// In case of any failure empty string is returned.
+// For n's meaning see [runtime.Caller] documentation.
+func NthCallerName(n int) string {
 	pc, _, _, ok := runtime.Caller(n)
 	if !ok {
 		return ""
@@ -20,11 +22,11 @@ func nthCallerName(n int) string {
 // CallerName returns name of the function that called CallerName.
 // In case of any failure empty string is returned.
 func CallerName() string {
-	return nthCallerName(2)
+	return NthCallerName(2)
 }
 
 // CallerCallerName returns name of the function that called the function that called CallerCallerName.
 // In case of any failure empty string is returned.
 func CallerCallerName() string {
-	return nthCallerName(3)
+	return NthCallerName(3)
 }
--- runtimehelper_test.go
+++ runtimehelper_test.go
@@ -1,9 +1,33 @@
 package runtimehelper
 
 import (
+	"path"
 	"runtime"
 	"testing"
 )
+
+func TestNthCallerName(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "0", args: args{n: 0}, want: "runtimehelper.NthCallerName"},
+		{name: "1", args: args{n: 1}, want: "runtimehelper.TestNthCallerName.func1"},
+		{name: "2", args: args{n: 2}, want: "testing.tRunner"},
+		{name: "3", args: args{n: 3}, want: "runtime.goexit"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := path.Base(NthCallerName(tt.args.n)); got != tt.want {
+				t.Errorf("NthCallerName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestCallerName(t *testing.T) {
 	t.Run("1", func(t *testing.T) {
